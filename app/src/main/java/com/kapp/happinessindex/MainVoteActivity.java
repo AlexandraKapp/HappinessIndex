8e91ef561aa67fc09f92f8c765005d069e6a37f1
Alexandra Kapp <alexandra.kapp@stud.uni-bamberg.de>
2017-03-07 15:15:36 +0100

    some layout changes

--- app/src/main/java/com/kapp/happinessindex/MainVoteActivity.java
+++ app/src/main/java/com/kapp/happinessindex/MainVoteActivity.java
@@ -1,29 +1,66 @@
 package com.kapp.happinessindex;
 
 import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 
-public class MainVoteActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static android.R.attr.id;
+import static com.kapp.happinessindex.R.id.vote_radio_button_group;
+
+public class MainVoteActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, RadioGroup.OnCheckedChangeListener {
 
     public static String SELECTED_HASHCODE_KEY = "hashcode key";
+
+    @SuppressWarnings("WeakerAccess")
+    @BindView(R.id.drop_down_menu)
     Spinner mDropDownMenu;
+
+
+    @SuppressWarnings("WeakerAccess")
+    @BindView(R.id.vote_radio_button_group)
+    RadioGroup mRadioGroup;
+
+    @SuppressWarnings("WeakerAccess")
+    @BindView(R.id.green_button)
+    Button greenButton;
+
+    @SuppressWarnings("WeakerAccess")
+    @BindView(R.id.orange_button)
+    Button orangeButton;
+
+    @SuppressWarnings("WeakerAccess")
+    @BindView(R.id.red_button)
+    Button redButton;
+
     String selectedHashCode;
+    final int GREEN_SELECTED = 1;
+    final int ORANGE_SELECTED = 2;
+    final int RED_SELECTED = 3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_vote);
-        mDropDownMenu = (Spinner) findViewById(R.id.drop_down_menu);
+
+        ButterKnife.bind(this);
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.hashcode_options, R.layout.support_simple_spinner_dropdown_item);
         adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         mDropDownMenu.setAdapter(adapter);
         mDropDownMenu.setOnItemSelectedListener(this);
+        mRadioGroup.setOnCheckedChangeListener(this);
 
 
     }
@@ -33,9 +70,10 @@ public class MainVoteActivity extends AppCompatActivity implements AdapterView.O
         //TODO: check if a button is selected
         //TODO: check if already voted
         //TODO: check if hashtag is selected
+
         //TODO: add vote to database
 
-        Log.e("DEBUG", "selected Hash Code " + selectedHashCode);
+        //getSelectedValue();
 
         Intent nextActivity = new Intent(MainVoteActivity.this, StatsActivity.class);
         nextActivity.putExtra(SELECTED_HASHCODE_KEY, selectedHashCode);
@@ -43,14 +81,76 @@ public class MainVoteActivity extends AppCompatActivity implements AdapterView.O
         startActivity(nextActivity);
     }
 
+    /*
+    returns the selected value green, orange or red
+    if no value is selected -1 is returned
+     */
+    private int getSelectedValue() {
+
+        //TODO: fix bug nullpointerexception
+
+        int selectedValueButton = mRadioGroup.getCheckedRadioButtonId();
+
+        if (selectedValueButton == greenButton.getId()) {
+            return GREEN_SELECTED;
+        } else if (selectedValueButton == orangeButton.getId()) {
+            return ORANGE_SELECTED;
+        } else if (selectedValueButton == redButton.getId()) {
+            return RED_SELECTED;
+        } else {
+            return -1;
+        }
+    }
+
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Log.d("DEBUG", "get selected item in onItemselected " + (String) parent.getItemAtPosition(position));
         selectedHashCode = (String) parent.getItemAtPosition(position);
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        int selectedValue = getSelectedValue();
+
+        switch (selectedValue) {
+            case GREEN_SELECTED:
+                greenButton.setBackground(getDrawable(R.drawable.gradient_green));
+                orangeButton.setBackground(getDrawable(R.color.basic_orange));
+                redButton.setBackground(getDrawable(R.color.basic_red));
+
+                greenButton.setElevation(getResources().getDimension(R.dimen.small_elevation));
+                orangeButton.setElevation(getResources().getDimension(R.dimen.no_elevation));
+                redButton.setElevation(getResources().getDimension(R.dimen.no_elevation));
+
+                break;
+            case ORANGE_SELECTED:
+                greenButton.setBackground(getDrawable(R.color.basic_green));
+                orangeButton.setBackground(getDrawable(R.drawable.gradient_orange));
+                redButton.setBackground(getDrawable(R.color.basic_red));
+
+                greenButton.setElevation(getResources().getDimension(R.dimen.no_elevation));
+                orangeButton.setElevation(getResources().getDimension(R.dimen.small_elevation));
+                redButton.setElevation(getResources().getDimension(R.dimen.no_elevation));
+
+                break;
+            case RED_SELECTED:
+                greenButton.setBackground(getDrawable(R.color.basic_green));
+                orangeButton.setBackground(getDrawable(R.color.basic_orange));
+                redButton.setBackground(getDrawable(R.drawable.gradient_red));
+
+
+                greenButton.setElevation(getResources().getDimension(R.dimen.no_elevation));
+                orangeButton.setElevation(getResources().getDimension(R.dimen.no_elevation));
+                redButton.setElevation(getResources().getDimension(R.dimen.small_elevation));
+                break;
+            default:
+                greenButton.setBackground(getDrawable(R.color.basic_green));
+                orangeButton.setBackground(getDrawable(R.color.basic_orange));
+                redButton.setBackground(getDrawable(R.color.basic_red));
+        }
     }
 }
--- app/src/main/java/com/kapp/happinessindex/TabFragment.java
+++ app/src/main/java/com/kapp/happinessindex/TabFragment.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -70,9 +69,9 @@ public class TabFragment extends Fragment {
         View view;
         if (mTabType == TAB_BUTTONS_KEY) {
             view = inflater.inflate(R.layout.tab_colorfields_fragment, container, false);
-            TextView greenField = (TextView) view.findViewById(R.id.green_button_stats);
-            TextView orangeField = (TextView) view.findViewById(R.id.orange_button_stats);
-            TextView redField = (TextView) view.findViewById(R.id.red_button_stats);
+            TextView greenField = (TextView) view.findViewById(R.id.green_button);
+            TextView orangeField = (TextView) view.findViewById(R.id.orange_button);
+            TextView redField = (TextView) view.findViewById(R.id.red_button);
             TextView totalVotes = (TextView) view.findViewById(R.id.total_votes);
 
            //TODO: exchange example test texts

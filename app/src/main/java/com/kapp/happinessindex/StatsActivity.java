ed5e45eda4d835aa4b6b1647735537d476b333cb
Alexandra Kapp <alexandra.kapp@stud.uni-bamberg.de>
2017-03-07 09:33:12 +0100

    Set up of stats layout

--- app/src/main/java/com/kapp/happinessindex/StatsActivity.java
+++ app/src/main/java/com/kapp/happinessindex/StatsActivity.java
@@ -3,11 +3,11 @@ package com.kapp.happinessindex;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainRateActivity extends AppCompatActivity {
+public class StatsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_vote);
+        setContentView(R.layout.activity_stats);
     }
 }

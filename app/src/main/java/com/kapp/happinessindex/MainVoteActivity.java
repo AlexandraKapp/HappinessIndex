3acd76f7e55302cf8bae27a950b1ff52dcf5e3eb
Alexandra Kapp <alexandra.kapp@stud.uni-bamberg.de>
2017-03-07 10:10:04 +0100

    set up drop down menu

--- app/src/main/java/com/kapp/happinessindex/MainVoteActivity.java
+++ app/src/main/java/com/kapp/happinessindex/MainVoteActivity.java
@@ -2,12 +2,22 @@ package com.kapp.happinessindex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainVoteActivity extends AppCompatActivity {
+
+    Spinner mDropDownMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_vote);
+        mDropDownMenu = (Spinner) findViewById(R.id.drop_down_menu);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.hashcode_options, R.layout.support_simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        mDropDownMenu.setAdapter(adapter);
+
+
     }
 }

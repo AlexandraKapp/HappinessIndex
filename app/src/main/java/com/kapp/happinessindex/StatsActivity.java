e41096ae6de6632dde1dabc2ed0fc23ed3a49fa4
Alexandra Kapp <alexandra.kapp@stud.uni-bamberg.de>
2017-03-07 12:40:56 +0100

    menu to make a new Vote from Stats Activity

--- app/src/main/java/com/kapp/happinessindex/StatsActivity.java
+++ app/src/main/java/com/kapp/happinessindex/StatsActivity.java
@@ -1,17 +1,20 @@
 package com.kapp.happinessindex;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class StatsActivity extends AppCompatActivity implements TabFragment.OnFragmentInteractionListener{
+public class StatsActivity extends AppCompatActivity implements TabFragment.OnFragmentInteractionListener {
 
     @SuppressWarnings("WeakerAccess")
     @BindView(R.id.hashtag_as_title)
@@ -42,7 +45,7 @@ public class StatsActivity extends AppCompatActivity implements TabFragment.OnFr
         if (getIntent().getExtras() != null) {
             String hashCode = getIntent().getStringExtra(MainVoteActivity.SELECTED_HASHCODE_KEY);
             mHashCode.setText(hashCode);
-            }
+        }
 
         mAtTeam.setText("@exampleTeam");
 
@@ -54,5 +57,17 @@ public class StatsActivity extends AppCompatActivity implements TabFragment.OnFr
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.make_new_vote, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        startActivity(new Intent(StatsActivity.this, MainVoteActivity.class));
+        return true;
     }
 }
